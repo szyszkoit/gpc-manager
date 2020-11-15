91b4e61918bc6ef124afb3490f4d4b1cefe4c2f2
Krzysztof Szyszko <k.szyszko14@gmail.com>
2020-11-15 18:54:25 +0100

    1

--- server/index.js
+++ server/index.js
@@ -23,6 +23,6 @@ if (process.env.NODE_ENV === 'production') {
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
